bb8cba3678955a1533515f486f08a39fd3a269ee
Vibhor Agarwal <agarwalvibhor99@gmail.com>
2020-07-03 20:19:59 -0400

    user route done

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user")
-
+const Order = require("../models/order")
 
 //Methods
 
@@ -25,13 +25,74 @@ exports.getUser = (req, res) => {
     return res.json(req.profile)
 }
 
-exports.getAllUser = (req, res) => {
-    User.find().exec((err, users) => {
-         if(err || !users){
-             return res.status(400).json({
-                 error: "No User Found"
-             })
-         }
-         res.json(users)
+exports.updateUser = (req, res) => {
+
+    User.findByIdAndUpdate(
+        {_id: req.profile._id},
+        {$set: req.body},
+        {new: true, useFindandModify: false},
+        (err, user) => {
+            if(err){
+                return res.tatus(400).json({
+                    error: "You are not authorized to update the user"
+                })
+            }
+            user.salt = undefined
+            user.encry_password = undefined;
+            res.json(user);
+        }
+    )
+}
+
+exports.userOrderList = (req, res) => {
+    Order.find({user: req.profile._id}).populate("user", "_id name").exec((err, order) => {
+        if(err){
+            return res.status(400).json({
+                error: "No order for this User"
+            })
+        }
+        return res.json(order)
     })
 }
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+
+    let purchases = []
+    req.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id
+        })
+    })
+
+    //store this in Database
+    User.findOneAndUpdate(
+        {_id: req.profile._id},
+        {$push: {purchases: purchases}},
+        {new: true},
+        (err, purchases) => {
+            if(err){
+                return res.status(400).json({
+                    error: "Unabale to save purcahse list"
+                })
+            }
+            next()
+        }
+    )
+}
+
+// exports.getAllUser = (req, res) => {
+//     User.find().exec((err, users) => {
+//          if(err || !users){   
+//              return res.status(400).json({
+//                  error: "No User Found"
+//              })
+//          }
+//          res.json(users)
+//     })
+// }
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const router = express.Router()
 
-const { getUserByID, getUser, getAllUser } = require("../controllers/user")
+const { getUserByID, getUser, updateUser, userOrderList } = require("../controllers/user")
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth")
 
 router.param("userId", getUserByID)
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser)
 
-router.get("/users", getAllUser)
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser)
+
+router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userOrderList)
+
+// router.get("/users", getAllUser) // TESTING : add in line 4 getAllUser if want to use
 module.exports = router
