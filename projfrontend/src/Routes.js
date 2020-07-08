a2d59e748eec68a4e980898778a081c12e3fa6d4
Vibhor Agarwal <agarwalvibhor99@gmail.com>
2020-07-08 19:26:16 -0400

    getting error in updateProduct

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -9,6 +9,9 @@ import UserDashBoard from "./user/UserDashBoard"
 import AdminDashBoard from "./user/AdminDashBoard"
 import AddProduct from "./admin/AddProduct"
 import AddCategory from "./admin/AddCategory"
+import ManageCategories from './admin/ManageCategories'
+import ManageProducts from './admin/ManageProducts'
+import UpdateProduct from './admin/UpdateProduct'
 
 const Routes = () => {
     return (
@@ -17,10 +20,13 @@ const Routes = () => {
                 <Route path="/" exact component= {Home} />
                 <Route path="/signup" exact component= {Signup} />                
                 <Route path="/signin" exact component= {Signin} />   
-                <PrivateRoute path="/user/dashboard" exact component= {UserDashBoard} />             
+                <PrivateRoute path="/user/dashboard " exact component= {UserDashBoard} />             
                 <AdminRoute path="/admin/dashboard" exact component= {AdminDashBoard} />
                 <AdminRoute path="/admin/create/product" exact component= {AddProduct} />
                 <AdminRoute path="/admin/create/category" exact component= {AddCategory} />
+                <AdminRoute path="/admin/categories" exact component= {ManageCategories} />
+                <AdminRoute path="/admin/products" exact component= {ManageProducts} />
+                <AdminRoute path="/admin/product/update/:productId" exact component= {UpdateProduct} />
                 
 
             </Switch>
--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -17,3 +17,88 @@ export const createCategory = (userId, token, category) => {
 }
 
 //success message to toastify
+
+//Get All Categories
+export const getAllCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: "GET",
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+
+//product calls
+
+export const createProduct = (userId, token, product) => {
+    return fetch(`${API}/product/create/${userId}`, {
+        method: "POST",
+        headers:{
+            Accept: "application/json",
+            Authorization: `Bearer ${token}` 
+        },
+        body: product
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+
+//Get All Products
+export const getAllProducts = () => {
+    return fetch(`${API}/products`, {
+        method: "GET",
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+//Delete a produce
+export const deleteProduct = (productId, userId, token) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: "DELETE",
+        headers:{
+            Accept: "application/json",
+            Authorization: `Bearer ${token}` 
+        }  
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+
+//get a product
+export const getProduct = productId => {
+    return fetch(`${API}/product/${productId}`, {
+      method: "GET"
+    })
+      .then(response => {
+        return response.json()
+      })
+      .catch(err => console.log(err))
+  }
+
+
+//update a product
+export const updateProduct = (productId, userId, token, product) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: "PUT",
+        headers:{
+            Accept: "application/json",
+            Authorization: `Bearer ${token}` 
+        },
+        body: product
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
